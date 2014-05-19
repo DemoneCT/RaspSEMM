29551f6be3fd9cccbfcb3e9156ee3bf18c222993
DemoneCT <fbasile83@gmail.com>
2014-05-19 14:46:39 +0200

    Commit Fittizio

--- app/src/main/java/com/rasp/raspsemm/app/MainActivity.java
+++ app/src/main/java/com/rasp/raspsemm/app/MainActivity.java
@@ -18,6 +18,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+
 import java.util.List;
 import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
